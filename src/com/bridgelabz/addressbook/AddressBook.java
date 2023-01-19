ebdb5ed0a757df6a02799534034abee54cd7ccec
shiwani <shiwanipaunikar1@gmail.com>
2023-01-19 08:59:37 -0800

    Added:creating contact

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBook {
+    //Declaring variables for Address Book System
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
+
+    //Declaring a parameterized constructor by means of which we can set up the values for instance variables above.
+    public AddressBook(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    //Creating toString methods to grab the values.
+
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -3,7 +3,7 @@ package com.bridgelabz.addressbook;
 public class AddressBookMain {
 
     public static void main(String[] args) {
-        Contact addressBook = new Contact("Shiv","Paunikar","Nandanvan","Nagpur","MH","9478372627","441567","shiv@gmail.com");
+        AddressBook addressBook = new AddressBook("Shiv","Paunikar","Nandanvan","Nagpur","MH","9478372627","441567","shiv@gmail.com");
         System.out.print("\n"+addressBook.toString());
 
 
